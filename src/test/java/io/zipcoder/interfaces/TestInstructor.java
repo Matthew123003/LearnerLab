f94d70627aa9e952d9d0fa9405ba0ad26c67ecc2
Matt Kramer <matthew123003@gmail.com>
2024-04-12 09:08:22 -0400

    Working on 5.0

--- src/test/java/io/zipcoder/interfaces/TestInstructor.java
+++ src/test/java/io/zipcoder/interfaces/TestInstructor.java
@@ -1,4 +1,34 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+
 public class TestInstructor {
+
+    @Test
+    public void testImplementation(){
+        Instructor instructor = new Instructor(1, "Matt");
+
+        Assert.assertTrue(instructor instanceof Teacher);
+    }
+
+    @Test
+    public void testInheritance(){
+        Instructor instructor = new Instructor(1, "Matt");
+
+        Assert.assertTrue(instructor instanceof Person);
+    }
+
+    @Test
+    public void testTeach(){
+        Instructor instructor = new Instructor(1, "Matt");
+        Student student = new Student(1, "Matt", 0.0);
+        double expected = 1;
+    }
+
+    @Test
+    public void testLecture(){
+
+    }
 }
