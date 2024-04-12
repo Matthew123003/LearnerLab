8e85a933ca13e2b09f0491f9c4352caa4329e9bf
Matt Kramer <matthew123003@gmail.com>
2024-04-12 08:45:12 -0400

    Working on Part 5

---
+++ src/main/java/io/zipcoder/interfaces/Instructor.java
@@ -0,0 +1,17 @@
+package io.zipcoder.interfaces;
+
+public class Instructor extends Person implements Teacher{
+    public Instructor(long id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void teach(Learner learner, double numberOfHours) {
+
+    }
+
+    @Override
+    public void lecture(Learner[] learners, double numberOfHours) {
+
+    }
+}
