a76a9b1986185ebbbdea4210b082a94d3f444fb4
Matt Kramer <matthew123003@gmail.com>
2024-04-12 08:17:44 -0400

    Part 1 done

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,33 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void personConstructorTest(){
+        long expectedId = 1;
+        String expectedName = "Matt";
+        Person person = new Person(expectedId, expectedName);
+
+        long actualId = person.getId();
+        String actualName = person.getName();
+
+        Assert.assertEquals(expectedId, actualId);
+        Assert.assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void setNameTest(){
+        String expected = "Matt";
+
+        Person person = new Person(1, null);
+        person.setName(expected);
+        String actual = person.getName();
+
+        Assert.assertEquals(expected, actual);
+    }
 
 }
