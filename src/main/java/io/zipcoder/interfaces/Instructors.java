b8b8066948a6375dae24c3fca2be6f2d41df8aee
Matt Kramer <matthew123003@gmail.com>
2024-04-12 15:30:27 -0400

    Part 8 Finished

---
+++ src/main/java/io/zipcoder/interfaces/Instructors.java
@@ -0,0 +1,17 @@
+package io.zipcoder.interfaces;
+
+public final class Instructors extends People{
+    private static final Instructors inst = new Instructors();//single instantiation of Instructors object
+
+    private Instructors(){//private constructor
+        personList.add(new Instructor(1, "Dolio"));
+        personList.add(new Instructor(2, "Kris"));
+        personList.add(new Instructor(3, "Desa"));
+        personList.add(new Instructor(4, "Lossie"));
+        personList.add(new Instructor(5, "Dasha"));
+        personList.add(new Instructor(6, "Dan"));
+        personList.add(new Instructor(7, "Cassie"));
+    }
+
+    public static Instructors getInstance(){return inst;}//Gets the instance of instructor
+}
---
+++ src/test/java/io/zipcoder/interfaces/TestInstructors.java
@@ -0,0 +1,29 @@
+package io.zipcoder.interfaces;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestInstructors {
+
+    @Test
+    public void testInstructorsSingleton1(){
+        Instructors instructors = Instructors.getInstance();
+        List<Person> actualInstructors = instructors.personList;
+
+        List<Person> expectedInstructors = new ArrayList<>();
+        expectedInstructors.add(new Instructor(1, "Dolio"));
+        expectedInstructors.add(new Instructor(2, "Kris"));
+        expectedInstructors.add(new Instructor(3, "Desa"));
+        expectedInstructors.add(new Instructor(4, "Lossie"));
+        expectedInstructors.add(new Instructor(5, "Dasha"));
+        expectedInstructors.add(new Instructor(6, "Dan"));
+        expectedInstructors.add(new Instructor(7, "Cassie"));
+
+
+
+        Assert.assertEquals(expectedInstructors.size(), actualInstructors.size());
+    }
+}
