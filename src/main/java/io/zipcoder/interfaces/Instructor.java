4dd229c0790063b7337d52ce647d0d947c383a6c
Matt Kramer <matthew123003@gmail.com>
2024-04-12 09:01:18 -0400

    5.1 done, working on 5.0

--- src/main/java/io/zipcoder/interfaces/Instructor.java
+++ src/main/java/io/zipcoder/interfaces/Instructor.java
@@ -7,11 +7,14 @@ public class Instructor extends Person implements Teacher{
 
     @Override
     public void teach(Learner learner, double numberOfHours) {
-
+        learner.learn(numberOfHours);
     }
 
     @Override
     public void lecture(Learner[] learners, double numberOfHours) {
-
+        double evenSplitHours = numberOfHours / learners.length;
+        for(Learner pupil : learners){
+            pupil.learn(evenSplitHours);
+        }
     }
 }
