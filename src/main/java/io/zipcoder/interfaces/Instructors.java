62a647edcf8e2a52e7edb68e646c9625ac33bb1e
Matt Kramer <matthew123003@gmail.com>
2024-04-12 22:03:29 -0400

    Finished

--- src/main/java/io/zipcoder/interfaces/Instructors.java
+++ src/main/java/io/zipcoder/interfaces/Instructors.java
@@ -4,13 +4,14 @@ public final class Instructors extends People<Instructor>{
     private static final Instructors inst = new Instructors();//single instantiation of Instructors object
 
     private Instructors(){//private constructor
-        personList.add(new Instructor(1, "Dolio"));
-        personList.add(new Instructor(2, "Kris"));
-        personList.add(new Instructor(3, "Desa"));
-        personList.add(new Instructor(4, "Lossie"));
-        personList.add(new Instructor(5, "Dasha"));
-        personList.add(new Instructor(6, "Dan"));
-        personList.add(new Instructor(7, "Cassie"));
+//Commented out list adds because of Educator Enum class adding instance of instructor in constructor
+//        personList.add(new Instructor(1, "Dolio"));
+//        personList.add(new Instructor(2, "Kris"));
+//        personList.add(new Instructor(3, "Desa"));
+//        personList.add(new Instructor(4, "Lossie"));
+//        personList.add(new Instructor(5, "Dasha"));
+//        personList.add(new Instructor(6, "Dan"));
+//        personList.add(new Instructor(7, "Cassie"));
     }
 
     public static Instructors getInstance(){return inst;}//Gets the instance of instructor
--- src/test/java/io/zipcoder/interfaces/TestEducator.java
+++ src/test/java/io/zipcoder/interfaces/TestEducator.java
@@ -1,5 +1,48 @@
 package io.zipcoder.interfaces;
 
-public class TestEducator {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestEducator {
+    @Test
+    public void testImplementation(){
+        Educator educator = Educator.DOLIO;
+
+        Assert.assertTrue(educator instanceof Teacher);
+    }
+
+//    @Test
+//    public void testInheritance(){
+//        Educator educator = Educator.DOLIO;
+//
+//        Assert.assertTrue(educator instanceof Person);
+//    }
+
+    @Test
+    public void testTeach(){
+        Educator educator = Educator.DOLIO;
+        Student student = new Student(1, "Matt", 0.0);
+        double expected = 1;
+
+        educator.teach(student, 1.0);
+        double actual = student.getTotalStudyTime();
+
+        Assert.assertEquals(expected, actual, .00001);
+    }
+
+    @Test
+    public void testLecture(){
+        Educator educator = Educator.DOLIO;
+        Student student1 = new Student(1, "Matt", 0.0);
+        Student student2 = new Student(2, "Jake", 0.0);
+        Learner[] learners = {student1, student2};
+        double expected = 1.0;
+
+        educator.lecture(learners, 2.0);
+        double actual1 = student1.getTotalStudyTime();
+        double actual2 = student2.getTotalStudyTime();
+
+        Assert.assertEquals(expected, actual1, .00001);
+        Assert.assertEquals(expected, actual2, .00001);
+    }
 }
