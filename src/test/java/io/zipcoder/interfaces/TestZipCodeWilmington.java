1288f54a783bc2ccca1029b282e0b0d08f8b6a0d
Matt Kramer <matthew123003@gmail.com>
2024-04-12 20:28:11 -0400

    Part 9 Finished

--- src/test/java/io/zipcoder/interfaces/TestZipCodeWilmington.java
+++ src/test/java/io/zipcoder/interfaces/TestZipCodeWilmington.java
@@ -1,11 +1,50 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.Optional;
 
 public class TestZipCodeWilmington {
 
     @Test
-    public void testHostLecture(){
+    public void testHostLecture() {
+        // Create a ZipCodeWilmington instance
+        ZipCodeWilmington zipCodeWilmington = ZipCodeWilmington.getInstance();
 
+        // Get the instance of students
+        Students students = ZipCodeWilmington.getStudents();
+
+        // Get the list of students
+        List<Person> studentList = students.personList;
+
+        // Choose an instructor
+        Instructor instructor = (Instructor) ZipCodeWilmington.getInstructors().findById(1);
+
+        // Specify the number of hours
+        double numberOfHours = 10.0;
+
+        // Store initial totalStudyTime of each student
+        double[] initialStudyTimes = new double[studentList.size()];
+        for (int i = 0; i < studentList.size(); i++) {
+            Student student = (Student) studentList.get(i);
+            initialStudyTimes[i] = student.getTotalStudyTime();
+        }
+
+        // Invoke the hostLecture method
+        zipCodeWilmington.hostLecture(instructor, numberOfHours);
+        double expected = 0;
+        double actual = 0;
+        // Check each student's totalStudyTime
+        for (int i = 0; i < studentList.size(); i++) {
+            Student student = (Student) studentList.get(i);
+            double expectedStudyTime = initialStudyTimes[i] + numberOfHours / studentList.size();
+            expected = expectedStudyTime;
+            actual = student.getTotalStudyTime();
+        }
+
+        Assert.assertEquals(expected, actual, .00001);
     }
 }
+
